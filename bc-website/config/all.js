beda74cfade342aa4ea72db6f85397bdfba589db
Abhilash Reddy <abhilash4252@gmail.com>
2019-11-25 18:43:57 +0530

    First commit

---
+++ bc-website/config/all.js
@@ -0,0 +1,15 @@
+module.exports = exports = {
+  port: "4000",
+  theme: "basic",
+  languages: [
+    {
+      "code": "en-us",
+      "relative_url_prefix": "/"
+    }
+  ],
+  plugins: {},
+  contentstack: {
+    api_key: "blt4393bfb360e3c9a9",
+    access_token: "blt53eae14ce6c038fa"
+  }
+};
